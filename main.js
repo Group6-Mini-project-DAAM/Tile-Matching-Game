22b0b2af933ab4dd1c2912244100b2f167b579b9
AmanPatne <147914792+AmanPatne@users.noreply.github.com>
2024-03-16 11:32:27 +0530

    javascript Updated

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+//These functions help add, remove or toggle css classes
 
 function tog_class(id, cl) {
   var elem = document.getElementById(id);
@@ -23,7 +23,7 @@ function rem_class(id, cl) {
   }
 }
 
-
+//This function adds/removes the selected class of a selected tile
 
 function tog_elem(i, elem) {
   document.getElementById("tile_" + i).onclick = function() {
@@ -34,7 +34,7 @@ function tog_elem(i, elem) {
   };
 }
 
-
+//This function adds the selected class of a selected tile
 
 function add_elem(i, elem) {
   document.getElementById("tile_" + i).onclick = function() {
@@ -44,34 +44,35 @@ function add_elem(i, elem) {
 
     if (window.tilecount == 1) {
       if (i != window.lasttile && window.A[i] == window.A[window.lasttile]) {
-        
-        var first = document.getElementById("tile_" + i); 
-        var second = document.getElementById("tile_" + window.lasttile); 
+        //checks if the 2 stmbols match and aren't the same tile
+        var first = document.getElementById("tile_" + i); //get tile id
+        var second = document.getElementById("tile_" + window.lasttile); //get tile id
 
-        first.classList.add("tile_closed"); 
-        second.classList.add("tile_closed"); 
+        first.classList.add("tile_closed"); //remove tiles
+        second.classList.add("tile_closed"); //remove tiles
 
-        first.onclick = ""; 
-        second.onclick = ""; 
+        first.onclick = ""; //remove event handlers
+        second.onclick = ""; //remove event handlers
 
-        window.paircount++; 
+        window.paircount++; //increment pair count
         if (window.paircount == 8) {
-         
+          //win condition
+          //alert('You win!');//show victory banner here
           add_class("overlay_win", "overlay_win_open");
         }
       } else {
-        rem_delay(window.opentile, window.lasttile);
+        rem_delay(window.opentile, window.lasttile); //clears the tiles with a 1 second delay(to let the player see the tile)
       }
 
-      window.tilecount = 0; 
+      window.tilecount = 0; //resets the opened tile counter to 0
     } else {
-      window.lasttile = i; 
+      window.lasttile = i; //sets the last tile
       window.tilecount++;
-    }
+    } //increments the tile count
   };
 }
 
-
+//This function removes all tile classes (used to hide tiles)
 
 function rem_select(i) {
   rem_class("tile_" + i, "tile_open");
@@ -81,16 +82,17 @@ function rem_select(i) {
   rem_class("tile_icon_" + i, "fa-diamond");
 }
 
-
+//this function hides tiles, with a delay
+//it is called when the tiles don't match
 
 function rem_delay(first, second) {
   setTimeout(function() {
-    rem_select(first); 
-    rem_select(second); 
+    rem_select(first); //closes open tiles
+    rem_select(second); //closes open tiles
   }, 1000);
 }
 
-
+//This function shuffles the tiles
 
 function shuffle() {
   var j;
@@ -125,42 +127,42 @@ function shuffle() {
   return A;
 }
 
-
+//this function resets all tiles
 
 function reset_tiles() {
   for (i = 0; i < 16; i++) {
-    rem_select(i); 
-    add_elem(i, A[i]); 
-    rem_class("tile_" + i, "tile_closed"); .
+    rem_select(i); //turns around tiles
+    add_elem(i, A[i]); //adds events
+    rem_class("tile_" + i, "tile_closed"); //Tiles back to normal opacity..
   }
 }
 
 //this function resets the game
 
 function reset() {
-  window.A = shuffle(); 
+  window.A = shuffle(); //shuffle tiles and reset variables
   window.paircount = 0;
   window.tilecount = 0;
   window.lasttile = null;
   window.opentile = null;
 
-  reset_tiles(); 
+  reset_tiles(); //reset all tiles
 
-  rem_class("overlay_win", "overlay_win_open"); 
+  rem_class("overlay_win", "overlay_win_open"); //hide victory banner
 }
 
 //variables
 
-var A = shuffle(); 
-var paircount = 0; 
-var tilecount = 0; 
-var lasttile = null; 
-var opentile = null;
+var A = shuffle(); //shuffle the tiles
+var paircount = 0; //tracks the number of matches
+var tilecount = 0; //tracks the number of open tiles
+var lasttile = null; //tracks the last open tile
+var opentile = null; //tracks the current open tile
 
 //do stuff here
 
 for (i = 0; i < 16; i++) {
-  add_elem(i, A[i]);
+  add_elem(i, A[i]); //adds event handlers to the tiles.
 }
 
 document.getElementById("overlay_win").onclick = function() {
